24b53205b777da0cbeec194b08d25e76f6049af5
mook140853 <27346@bbw.ac.th>
2026-06-11 14:23:16 +0700

    โปรแกรมในการคำนวณการใช้จ่าย เวอร์ชั่น1
    เริ่มเข้าใจได้บ้างแล้วค่ะแต่อาจจะต้องใช้เวลา 11มิ.ย69  ห้อง4/4  ฝึกวิเคราะห์ lpo

---
+++ bujed39.py
@@ -0,0 +1,7 @@
+pricel =float(input("ราคาสินค้า 1"))
+amount1 =int(input("จำนวนสินค้า1" ))
+pricel2 = float(input("ราคาสินค้า2" ))
+amount2 = int(input("จำนวนสินค้า2" ))
+total =(pricel * amount1)+ ( pricel2 * amount2)
+print ("ค่าใช้จ่ายของคุณครั้งนี้ /nคือ ",total, " บาท")
+             
--- rakrin39.py
+++ rakrin39.py
@@ -1,4 +1,10 @@
-print('MOOK')
-print('I AM 15 years old')
-print(' I like play a games')
-print('I like sleeping')
+name = input("คุณชื่ออะไร? ")
+age = int( input("อายุเท่าไร?" ))
+print("สวัสดี", name)
+print("อีก 1 ปี คุณจะอายุ", age + 1, "ปี")
+a = 10 
+b = 2.5
+c = input("ใส่ข้อความ: ")
+print(type(a))
+print(type(b))
+print(type(c))

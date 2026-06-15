addb5acdbc299e2e837d6bc60fe20e684d31edf0
mook140853 <27346@bbw.ac.th>
2026-06-15 09:42:24 +0700

    Add files via upload
    ได้รู้การเขียนโปรมแกรมและได้รู้แต่ตัวของโค้ด i-p-o ของโปรแกรมเลขคู่เลขคี่ 15/06/69

---
+++ 39mook_15-06-69.py
@@ -0,0 +1,8 @@
+number =int(input("Enter your number : "))
+if number %2 == 0 :
+    print("Even Numbers")
+else:
+    print("odd numbers")   
+print("โปรแกรมเลขคู่หรือเลขคี่กันนะ")
+print(" by mook 4/4")
+print(" thank you")

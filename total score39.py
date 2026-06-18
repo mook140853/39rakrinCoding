4e41ab8da2ad1217d2335e8c4c595fe0daf8ab71
mook140853 <27346@bbw.ac.th>
2026-06-18 14:05:36 +0700

    โปแกรมคำนวณคะแนนรวม รับคะแน3วิชา
    ได้รู้การเขียนโค้ดว่าต้องใช้ยังไงตอนนี้อาจจะมีไม่ค่อยเข้าใจ และสนุก 18/06/69

---
+++ total score39.py
@@ -0,0 +1,11 @@
+point1 =float(input("คะแนนวิชาที่1"))
+point2 =float(input("คะแนนวิชาที่2" ))
+point3 = float(input("คะแนนวิชาที่3" ))
+total_point = (point1 + point2 + point3)
+average = total_point /3
+print ("คะแนนรวมของคุณ")
+if average < 60:
+  print("คะแนนรวมของคุณ =", total_point)
+  print("คะแนนเฉลี่ย 3 วิชา =" , average )
+  print("คะแนนรวมของคุณ")
+print("ควรปรับปรุง")
